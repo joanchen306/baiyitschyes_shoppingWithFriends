17e346c78bdd7b58c9b9c6311d66c3b3cb116114
Fujiyama <catherinemorris@lawn-128-61-44-244.lawn.gatech.edu>
2015-01-20 23:21:02 -0500

    changed Person 5

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,11 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+        String removed = input.substring(0,1);
+        String rest = input.substring(2);
+        String output = rest.concat(removed);
+        return output;
+
 	}
 	
 	/**
