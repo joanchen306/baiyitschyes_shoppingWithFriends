8b34c01be14b3a787e9e0fc26ab335211c48a722
Joan Chen <jkchen720@gmail.com>
2015-01-20 23:10:21 -0500

    commit to person4

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Tseng-Jung Chen
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,12 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        int n = input.length() - 1;
+        String reStr = "";
+        while (n >= 0) {
+            reStr = (char)((int)input.charAt(n) + 1) + reStr;
+        }
+        return reStr;
     }
     
     /**
