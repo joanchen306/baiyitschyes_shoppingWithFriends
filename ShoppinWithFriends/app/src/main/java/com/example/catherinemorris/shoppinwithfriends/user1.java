2f1bb5eacce6fdf99c32b74d08e5baa239b6f862
Joan Chen <jkchen720@gmail.com>
2015-02-03 17:50:46 -0500

    Adding user1 for registration

---
+++ ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/user1.java
@@ -0,0 +1,11 @@
+package com.example.catherinemorris.shoppinwithfriends;
+
+/**
+ * This is a temporary user profile for the application
+ * author: Joan Chen
+ * version: 1.0
+ */
+public class user1 {
+
+
+}
