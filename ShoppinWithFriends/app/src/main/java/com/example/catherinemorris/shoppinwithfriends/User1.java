7232a01dad5f15b98ff45a6472f429f5f172fc5b
Joan Chen <jkchen720@gmail.com>
2015-02-18 13:55:37 -0500

    switching db from mongo to firebase

--- ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/User1.java
+++ ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/User1.java
@@ -1,56 +1,24 @@
 package com.example.catherinemorris.shoppinwithfriends;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import com.firebase.client.Firebase;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.MongoClientURI;
-import org.bson.Document;
-import org.bson.types.ObjectId;
+
+
+
+
+
 
 /**
  * The user1 class creates static lists of usernames and passwords when a user registers
  *This will be transferred into the database 
  */
-public class User1 {
+public class User1 extends android.app.Application{
 
-    //connecting to database right here
-    MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-
-
-    public void addUser(String un, String pw, String em) {
-        MongoDatabase database = mongoClient.getDatabase( "baiyitschyes" );
-        MongoCollection<Document> collection = database.getCollection("users");
-        Document doc = new Document("username", un)
-                .append("name", un)
-                .append("password", pw)
-                .append("email", em)
-                .append("sales", 0)
-                .append("rating", -1);
-        collection.insertOne(doc);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+        // other setup code
     }
-
-    public boolean findUsername(String um) {
-        MongoDatabase database = mongoClient.getDatabase( "baiyitschyes" );
-        MongoCollection<Document> collection = database.getCollection("users");
-        Document myDoc = collection.find(new Document("username", um)).first();
-        if (myDoc != null) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean findPassWord(String pw) {
-        MongoDatabase database = mongoClient.getDatabase( "baiyitschyes" );
-        MongoCollection<Document> collection = database.getCollection("users");
-        Document myDoc = collection.find(new Document("password", pw)).first();
-        if (myDoc != null) {
-            return true;
-        }
-        return false;
-    }
-
-
+    
 }
