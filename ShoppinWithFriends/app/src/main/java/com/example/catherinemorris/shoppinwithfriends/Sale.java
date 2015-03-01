1576bd800ea2beaada0c0e1fa0181708054dc6a1
caicai67 <caitlin67@gmail.com>
2015-03-01 00:37:48 -0500

    added new sale class and request item activity

---
+++ ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/Sale.java
@@ -0,0 +1,9 @@
+package com.example.catherinemorris.shoppinwithfriends;
+
+/**
+ * A request that has been made for the sale of a particular item
+ * Created by Caitlin Morris on 2/28/15.
+ */
+public class Sale {
+
+}
