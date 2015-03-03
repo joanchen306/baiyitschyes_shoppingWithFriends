9bc7cf4bbbcde2985e4137d80aa846c8b871b56d
caicai67 <caitlin67@gmail.com>
2015-03-02 21:31:10 -0500

    joan did some stuff

--- ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/Sale.java
+++ ShoppinWithFriends/app/src/main/java/com/example/catherinemorris/shoppinwithfriends/Sale.java
@@ -6,4 +6,20 @@ package com.example.catherinemorris.shoppinwithfriends;
  */
 public class Sale {
 
+    private String item;
+    private String descripton;
+    private long min;
+    private long max;
+    private User user;
+
+    private UserDB db = new UserDB();
+
+    public Sale(String it, String des, long max, long min, User u) {
+        item = it;
+        descripton = des;
+        this.max = max;
+        this.min = min;
+        user = u;
+    }
+
 }
