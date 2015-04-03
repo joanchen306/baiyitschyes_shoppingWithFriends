e5d9f1bbb3c8c40ec78feea7fd1766745c514fed
jnugent6 <jnugent6@gatech.edu>
2015-04-03 12:57:25 -0400

    My test

--- ShoppinWithFriends/app/src/androidTest/java/com/example/catherinemorris/shoppinwithfriends/FriendListTest.java
+++ ShoppinWithFriends/app/src/androidTest/java/com/example/catherinemorris/shoppinwithfriends/FriendListTest.java
@@ -1,19 +1,34 @@
 package com.example.catherinemorris.shoppinwithfriends;
 
-import junit.framework.TestCase;
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class FriendListTest extends TestCase {
+
+public class FriendListTest extends ActivityInstrumentationTestCase2<FriendList> {
+
+    EditText friendName;
+    User userTester;
+    String friend;
+    String message;
+    Activity myActivity;
+
+    public FriendListTest() {
+        super(FriendList.class);
+    }
 
     public void setUp() throws Exception {
         super.setUp();
-
+        myActivity = getActivity();
+        friendName = (EditText) myActivity.findViewById(R.id.userText);
+        //friend = friendName.getText().toString();
+        //message = getMessage();
     }
 
-    public void tearDown() throws Exception {
-
+    public void noName() {
+        Button pressMe = (Button) myActivity.findViewById(R.id.addFriend);
     }
 
-    public void testAddFriends() throws Exception {
 
-    }
 }
